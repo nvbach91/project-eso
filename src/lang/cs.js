e34b2da4d5c4e090d11e678f89c2a496693eedf9
honza.do <honza.do@logio.cz>
2019-05-10 15:23:21 +0200

    lang - 1st pass

---
+++ src/lang/cs.js
@@ -0,0 +1,40 @@
+App.GLocaleCS {
+  "entry_btn"                               : "Začít s nákupem",
+
+  "welcome_title"                           : "Vítejte!",
+  "special_offer_btn"                       : "Přejít k nabídce!",
+  "eat_in_title"                            : "Na místě",
+  "eat_in_text"                             : "Chtěl bych jíst v pohodlí restaurace",
+  "eat_in_btn"                              : "Budu jíst v restauraci",
+  "take_out_title"                          : "S sebou",
+  "take_out_text"                           : "Chtěl bych si své jídlo vzít s sebou",
+  "take_out_btn"                            : "Zabalit jídlo domů",
+
+  "products_order_btn"                      : "Vybrat",
+  "checkout_btn"                            : "Přejít k objednávce",
+
+  "modal_product_detail"                    : "Detail produktu",
+
+  "modal_order_title"                       : "Vaše objednávka",
+  "modal_cancel_order_btn"                  : "Zrušit objednávku",
+  "modal_order_btn"                         : "Objednat   ",
+  "items"                                   : "položky/ek",
+
+  "checkout_title"                          : "Jakým způsobem chcete zaplatit?",
+  "checkout_card_pay_title"                 : "Platba kartou",
+  "checkout_card_pay_desc"                  : "Chci zaplatit přes kreditní kartu",
+  "checkout_card_pay_btn"                   : "Zaplatím kartou",
+  "checkout_cash_pay_title"                 : "Platba hotově",
+  "checkout_cash_pay_desc"                  : "Zaplatím to osobně u pultu",
+  "checkout_cash_pay_btn"                   : "Budu platit hotově",
+  "checkout_return_title"                   : "Zapomněli jste něco?",
+  "checkout_return_btn"                     : "Vrátit se",
+
+  "modal_timeout_title"                     : "Jste pořád tady?",
+  "modal_timeout_text"                      : "Prosíme Vás, dejte nám vědět, jestli jste pořád tady, jinak se aplikace vrátí na počáteční stránku.",
+  "modal_timeout_stay_btn"                  : "Ano, stále vybírám",
+  "modal_timeout_leave_btn"                 : "Ne, odejdu",
+
+  "close_btn"                               : "Zavřít",
+
+};
---
+++ src/lang/en.js
@@ -0,0 +1,42 @@
+App.GLocaleEN {
+  "entry_btn"                               : "Let\'s get started",
+
+  "welcome_title"                           : "Welcome!",
+  "special_offer_title"                     : "Daily offer: special on-the-house treatment",
+  "special_offer_desc"                      : "Order 5 get 1 free",
+  "special_offer_btn"                       : "Show me!",
+  "eat_in_title"                            : "Eat in",
+  "eat_in_text"                             : "Let\'s enjoy the atmosphere in our restaurant",
+  "eat_in_btn"                              : "I\'ll eat here",
+  "take_out_title"                          : "Take out",
+  "take_out_text"                           : "I want to pack my meal",
+  "take_out_btn"                            : "I\'ll take it home",
+
+  "products_order_btn"                      : "Order",
+  "checkout_btn"                            : "Checkout",
+
+  "modal_product_detail"                    : "Product details",
+
+  "modal_order_title"                       : "Your order",
+  "modal_cancel_order_btn"                  : "Cancel order",
+  "modal_order_btn"                         : "Order   ",
+  "items"                                   : "items",
+
+  "checkout_title"                          : "How would you like to pay?",
+  "checkout_card_pay_title"                 : "Pay by card",
+  "checkout_card_pay_desc"                  : "Simply use your credit card to pay",
+  "checkout_card_pay_btn"                   : "I\'ll pay by card",
+  "checkout_cash_pay_title"                 : "Pay in cash",
+  "checkout_cash_pay_desc"                  : "Even up in person at checkout",
+  "checkout_cash_pay_btn"                   : "I\'d like to pay in cash",
+  "checkout_return_title"                   : "Forgot something?",
+  "checkout_return_btn"                     : "Go back",
+
+  "modal_timeout_title"                     : "Are you still browsing?",
+  "modal_timeout_text"                      : "Please let us know you are still here. Otherwise it will go back to the initial screen.",
+  "modal_timeout_stay_btn"                  : "Yes, gimme a minute",
+  "modal_timeout_leave_btn"                 : "No, I\'ll leave",
+
+  "close_btn"                               : "Close",
+
+};
