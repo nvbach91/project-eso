f61e18ed6b5c7ab86a4af4e866a89070931aebee
honza.do <honza.do@logio.cz>
2019-06-26 21:18:23 +0200

    missed tab

--- src/js/admin/components/form.js
+++ src/js/admin/components/form.js
@@ -2,8 +2,8 @@ App.generateFormInput = (args) => {
   const { label, name, value, required, disabled } = args;
   return `
     <div class="form-group">
-        <label>${label}</label>
-        <input class="form-control" name="${name}" value="${value}"${required && ' required'} ${disabled && ' readonly'}>
+      <label>${label}</label>
+      <input class="form-control" name="${name}" value="${value}"${required && ' required'} ${disabled && ' readonly'}>
     </div>
   `;
 };
