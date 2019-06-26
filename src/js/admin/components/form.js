3fb64540ff8614cbb1262b20095905fcadfeeacf
honza.do <honza.do@logio.cz>
2019-06-26 21:15:16 +0200

    pull edits

--- src/js/admin/components/form.js
+++ src/js/admin/components/form.js
@@ -1,18 +1,9 @@
 App.generateFormInput = (args) => {
-  const { label, name, value, required } = args;
+  const { label, name, value, required, disabled } = args;
   return `
     <div class="form-group">
         <label>${label}</label>
-        <input class="form-control" name="${name}" value="${value}"${required && ' required'}>
-    </div>
-  `;
-};
-App.generateFormDisabledInput = (args) => {
-  const { label, name, value } = args;
-  return `
-    <div class="form-group">
-        <label>${label}</label>
-        <input class="form-control" name="${name}" value="${value}" readonly>
+        <input class="form-control" name="${name}" value="${value}"${required && ' required'} ${disabled && ' readonly'}>
     </div>
   `;
 };
