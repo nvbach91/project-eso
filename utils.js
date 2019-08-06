b0337432f1df1a37475a5f325bc0a115934bec9a
Nguyen Viet Bach <nvbach91@outlook.com>
2019-08-06 14:44:06 +0200

    fixing error handling

--- utils.js
+++ utils.js
@@ -1,7 +1,8 @@
 const utils = {
   handleError: (res) => (err) => {
     console.error(err);
-    res.status(err.code || 500).json({ msg: err.msg || (err.isOperational ? 'srv_invalid_certificate_password' : 'srv_internal_server_error') });
+    if (err.code) { res.status(err.code); }
+    res.json({ msg: err.msg || (err.isOperational ? 'srv_invalid_certificate_password' : 'srv_internal_server_error') || err });
   },
 };
 
